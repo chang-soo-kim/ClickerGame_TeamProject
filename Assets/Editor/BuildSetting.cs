524da7a321a5765093b11fd56af9cf334c85f266
KSR <cpflskfn@gmail.com>
2022-12-20 15:17:32 +0900

    Add Create Directory

--- Assets/Editor/BuildSetting.cs
+++ Assets/Editor/BuildSetting.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.IO;
+using System;
 
 public class BuildSetting
 {
     [MenuItem("MyTools/Build")]
     public static void MyBuild()
     {
-        string desktop = "C:/Users/User/Desktop";
-        string buildPath = desktop + "/Build/";
+        string desktop = "C:/MetaTrend_2";
+        string buildPath = desktop + "/MiniGame_2/";
         string[] scene = { "Assets/Scenes/SampleScene.unity" };
+        string folder = "";
 
         FileInfo buildInfo = new FileInfo(buildPath);
 
@@ -20,7 +22,16 @@ public class BuildSetting
             Directory.CreateDirectory(buildPath);
         }
 
+        folder = buildPath + DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss") + "/";
 
-        BuildPipeline.BuildPlayer(scene, buildPath + "build.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+        FileInfo folderInfo = new FileInfo(folder);
+
+        if (folderInfo.Exists == false) 
+        {
+            Directory.CreateDirectory(folder);
+        }
+
+
+        BuildPipeline.BuildPlayer(scene, folder + "build.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
     }
 }
